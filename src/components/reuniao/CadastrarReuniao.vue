7155bd8ea6a9c200ed7680f44cc1638b163df8f8
Otávio Guimarães <otaviovgsr@gmail.com>
2020-04-01 04:38:00 -0300

    Data e Time picker em versão dialog - Reuniao

--- src/components/reuniao/CadastrarReuniao.vue
+++ src/components/reuniao/CadastrarReuniao.vue
@@ -19,27 +19,98 @@
           </v-col>
 
           <v-col class="col-12" sm="6" md="6" lg="6">
-            <v-select label="Líder responsável" prepend-icon="mdi-account-star"></v-select>
+            <v-select
+              label="Líder responsável"
+              prepend-icon="mdi-account-star"
+            ></v-select>
           </v-col>
 
           <v-row justify="space-around">
-            <v-col class="col-12 column" sm="6" md="6" lg="6">
-              <span class="span-title">Data da Reunião</span>
-              <v-date-picker class="w-290" color="red lighten-1" locale="pt-br" v-model="picker"></v-date-picker>
+            <v-col cols="11" sm="5">
+              <v-dialog
+                ref="dialog2"
+                v-model="modal1"
+                :return-value.sync="date"
+                persistent
+                width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="date"
+                    label="Data do Evento"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  color="red lighten-1"
+                  locale="pt-br"
+                  scrollable
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="modal1 = false"
+                    >Voltar</v-btn
+                  >
+                  <v-btn text color="primary" @click="$refs.dialog2.save(date)"
+                    >OK</v-btn
+                  >
+                </v-date-picker>
+              </v-dialog>
             </v-col>
-            <v-col class="col-12 column" sm="6" md="6" lg="6">
-              <span class="span-title">Hora da Reunião</span>
-              <v-time-picker class="w-290" v-model="e7" color="red lighten-1" format="24hr"></v-time-picker>
+
+            <v-col cols="11" sm="5">
+              <v-dialog
+                ref="dialog"
+                v-model="modal2"
+                :return-value.sync="time"
+                persistent
+                width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="time"
+                    label="Hora do Evento"
+                    prepend-icon="mdi-clock-outline"
+                    readonly
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                  v-if="modal2"
+                  v-model="time"
+                  color="red lighten-1"
+                  format="24h"
+                  full-width
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="modal2 = false"
+                    >Cancel</v-btn
+                  >
+                  <v-btn text color="primary" @click="$refs.dialog.save(time)"
+                    >OK</v-btn
+                  >
+                </v-time-picker>
+              </v-dialog>
             </v-col>
           </v-row>
-            <v-col
+          <v-col
             class="col-12 offset-md-4 offset-lg-4 offset-sm-3 column-button"
             sm="6"
             md="6"
             lg="6"
           >
-            <v-btn class="ma-2" v-on:click.native="submit()" depressed color="success">Cadastrar</v-btn>
-            <v-btn class="ma-2" v-on:click.native="clear()" outlined>Limpar Campos</v-btn>
+            <v-btn
+              class="ma-2"
+              v-on:click.native="submit()"
+              depressed
+              color="success"
+              >Cadastrar</v-btn
+            >
+            <v-btn class="ma-2" v-on:click.native="clear()" outlined
+              >Limpar Campos</v-btn
+            >
           </v-col>
         </v-row>
       </v-form>
@@ -52,34 +123,37 @@ export default {
   data() {
     return {
       valid: true,
-      picker: new Date().toISOString().substr(0, 10),
+      date: new Date().toISOString().substr(0, 10),
+      modal1: false,
+      modal2: false,
+      time: null,
       e7: null,
       select: null
-    }
+    };
   },
 
   props: {
     show: Boolean
   }
-}
+};
 </script>
 
 <style scoped>
-  .w-290 {
-    max-width: 290px;
-  }
+.w-290 {
+  max-width: 290px;
+}
 
-  .span-title {
-    color: #777777;
-  }
+.span-title {
+  color: #777777;
+}
 
-  .column {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .column-button {
+.column {
   display: flex;
-    }
+  flex-direction: column;
+  align-items: center;
+}
+
+.column-button {
+  display: flex;
+}
 </style>
