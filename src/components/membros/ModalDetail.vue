21d9af6a487e5198bdfba4df73e0f51865acee83
Maria Clara Lima Santos <mariaclara@MacBook-Air-de-Maria.local>
2020-04-08 04:02:06 -0300

    edit member

--- src/components/membros/ModalDetail.vue
+++ src/components/membros/ModalDetail.vue
@@ -1,46 +1,107 @@
 <template>
   <v-dialog max-width="800px" persistent v-model="show">
     <v-card class="pa-5">
-      <v-layout row class="px-3" justify-space-between>
-        <h2>Detalhes do Membro:</h2>
+      <v-layout row class="px-3">
+        <v-btn v-if="!editMember" color="black" @click="editMember = true" title="Editar">
+          <v-icon color="white">mdi-account-edit</v-icon>
+        </v-btn>
+        <v-btn color="red" class="ml-2" title="Excluir">
+          <v-icon color="white">mdi-delete</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
         <v-btn @click="$emit('close')" title="Fechar" icon>
           <v-icon color="grey">mdi-close</v-icon>
         </v-btn>
       </v-layout>
-      <v-divider></v-divider>
-      <v-layout column class="mx-2 mt-2">
-          <v-layout row pl-1>
-                <span class="subheading font-weight-medium">Nome:</span>
-                <span class="subheading pl-2">{{ member.first_name }} {{ member.last_name }}</span>
+      <v-layout column mx-2 justify-center align-center>
+        <v-avatar size="130px" class="avatar">
+          <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          <v-btn class="float" v-if="editMember">
+            <v-icon>mdi-camera</v-icon>
+          </v-btn>
+        </v-avatar>
+        <span
+          v-if="!editMember"
+          class="title font-weight-medium mt-3"
+        >{{ member.first_name }} {{ member.last_name }}</span>
+        <span v-if="!editMember" class="subheading font-weight-regular">( {{ member.nickname }} )</span>
+        <v-layout row mt-3 justify-space-around style="width:100%">
+          <v-layout justify-left col-xs-12 col-sm-6 v-if="editMember">
+            <v-text-field
+              label="Nome"
+              outlined
+              prepend-inner-icon="mdi-alpha-n"
+              v-model="member.first_name"
+              hide-details
+            ></v-text-field>
           </v-layout>
-          <v-layout row pl-1>
-                <span class="subheading font-weight-medium">Email:</span>
-                <span class="subheading pl-2">{{ member.email }}</span>
+          <v-layout justify-left col-xs-12 col-sm-6 v-if="editMember">
+            <v-text-field
+              label="Sobrenome"
+              outlined
+              prepend-inner-icon="mdi-alpha-s"
+              v-model="member.last_name"
+              hide-details
+            ></v-text-field>
           </v-layout>
-          <v-layout row pl-1>
-                <span class="subheading font-weight-medium">Cargo:</span>
-                <span class="subheading pl-2">{{ member.post }}</span>
+          <v-layout justify-left col-xs-12 col-sm-6>
+            <v-text-field
+              outlined
+              prepend-inner-icon="mdi-email"
+              v-model="member.email"
+              :disabled="!editMember"
+              hide-details
+            ></v-text-field>
           </v-layout>
-          <v-layout row pl-1>
-                <span class="subheading font-weight-medium">Departamento:</span>
-                <span class="subheading pl-2">{{ member.department }}</span>
+          <v-layout justify-left col-xs-12 col-sm-6>
+            <v-text-field
+              outlined
+              prepend-inner-icon="mdi-slack"
+              v-model="member.slack"
+              :disabled="!editMember"
+              hide-details
+            ></v-text-field>
           </v-layout>
-          <v-layout row pl-1>
-                <span class="subheading font-weight-medium">Líder:</span>
-                <span class="subheading pl-2">{{ member.leader }}</span>
+          <v-layout justify-left col-xs-12 col-sm-6>
+            <v-text-field
+              outlined
+              prepend-inner-icon="mdi-phone"
+              v-model="member.phone"
+              :disabled="!editMember"
+              hide-details
+            ></v-text-field>
           </v-layout>
-          <v-layout row pl-1>
-                <span class="subheading font-weight-medium">Slack:</span>
-                <span class="subheading pl-2">{{ member.slack }}</span>
+          <v-layout justify-left col-xs-12 col-sm-6>
+            <v-text-field
+              outlined
+              prepend-inner-icon="mdi-briefcase"
+              v-model="member.post"
+              :disabled="!editMember"
+              hide-details
+            ></v-text-field>
           </v-layout>
-          <v-layout row pl-1>
-                <span class="subheading font-weight-medium">Telefone:</span>
-                <span class="subheading pl-2">{{ member.phone }}</span>
+          <v-layout justify-left col-xs-12 col-sm-6>
+            <v-text-field
+              outlined
+              prepend-inner-icon="mdi-border-none-variant"
+              v-model="member.area"
+              :disabled="!editMember"
+              hide-details
+            ></v-text-field>
           </v-layout>
-          <v-layout row pl-1>
-                <span class="subheading font-weight-medium">Data de Entrada:</span>
-                <span class="subheading pl-2">{{ data }}</span>
+          <v-layout justify-left col-xs-12 col-sm-6>
+            <v-text-field
+              outlined
+              prepend-inner-icon="mdi-account-star"
+              v-model="member.leader"
+              :disabled="!editMember"
+              hide-details
+            ></v-text-field>
           </v-layout>
+        </v-layout>
+        <v-layout row align-center v-if="editMember">
+          <v-btn class="ma-2" @click="sendEdit()" depressed color="success">Salvar</v-btn>
+        </v-layout>
       </v-layout>
     </v-card>
   </v-dialog>
@@ -50,25 +111,40 @@
 import moment from "moment";
 
 export default {
-    created(){
-        this.formatDate()
-    },
+  created() {
+    this.formatDate();
+  },
 
-    props:{
-        show: Boolean,
-        member: Object
-    },
+  props: {
+    show: Boolean,
+    member: Object
+  },
 
-    data(){
-        return{
-            data: null
-        }
-    },
+  data() {
+    return {
+      data: null,
+      editMember: false
+    };
+  },
 
-    methods:{
-        formatDate(){
-            this.data = moment(this.member.date_joined).format("DD/MM/YYYY")
-        }
+  methods: {
+    formatDate() {
+      this.data = moment(this.member.date_joined).format("DD/MM/YYYY");
+    },
+    sendEdit(){
+
     }
-}
+  }
+};
 </script>
+
+<style scoped>
+.avatar {
+  position: relative;
+}
+
+.float {
+  position: absolute;
+  top: 55px;
+}
+</style>
