bd0b002de0910d872556c319d8c40f0b16b1ce74
Daniel Lyncon <43709270+daniellyncon@users.noreply.github.com>
2020-04-29 16:14:57 -0300

    criação page critério de membresia

---
+++ src/controllers/MembershipCriteria.js
@@ -0,0 +1,16 @@
+const url = "membershipCriteria/";
+
+export default {
+    listMembershipCriteria(api) {
+        const response = api
+      .get(url)
+      .then(function(response) {
+        console.log(response);
+        return response;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+      return response;
+    },
+}
