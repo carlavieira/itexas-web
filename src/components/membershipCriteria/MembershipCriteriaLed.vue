49c51628a2c88276c894f88b33c4eb2fe3872d3a
Carla Vieira <carla.vieira@mlb.com>
2020-05-19 01:06:15 -0300

    Atualiza  critério de membresia dos liderados

--- src/components/membershipCriteria/MembershipCriteriaLed.vue
+++ src/components/membershipCriteria/MembershipCriteriaLed.vue
@@ -9,20 +9,12 @@
       no-data-text="Nenhum resultado até o momento"
       no-results-text="Nenhum resultado até o momento"
       :headers="header"
-      :items="membresia"
+      :items="filterLed"
     >
       <template v-slot:item.member="{ item }">
         {{
         item.member.first_name + " " + item.member.last_name
         }}
-      </template>
-      <template v-slot:item.member.leader="{ item }">
-        <span v-if="item.member.leader">
-          {{
-          item.member.leader.first_name
-          }}
-        </span>
-        <span v-else>-</span>
       </template>
       <template v-slot:item.dayMonth="{ item }">
         <span v-if="item.dayMonth">{{ formatDate(item.dayMonth) }}</span>
@@ -106,35 +98,41 @@ export default {
       MembershipCriteriaController,
       header: [
         { text: "Nome", value: "member" },
-        { text: "Área", value: "member.department.name" },
-        { text: "Líder", value: "member.leader" },
         { text: "Referência", value: "dayMonth" },
         { text: "Office Hours", value: "officeHoursCriteria" },
         { text: "Reuniões", value: "meetingsCriteria" },
         { text: "Eventos", value: "eventsCriteria" },
         { text: "Status", value: "status" }
       ],
-      membresia: [],
+      filterLed: [],
       showModal: false
     };
   },
 
   async created() {
-    await this.getMyCriteria();
+    await this.listMembershipCriteria();
   },
 
   methods: {
-    async getMyCriteria() {
-      this.MembershipCriteriaController.getMyMembershipCriteria(
-        this.$api,
-        localStorage.getItem("user_id")
-      )
-        .then(res => {
+    async listMembershipCriteria() {
+      await this.MembershipCriteriaController.listMembershipCriteria(this.$api)
+        .then((res) => {
           this.membresia = res.data;
+          this.filter()
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
+    },
+
+    filter(){
+      this.membresia.forEach(criteria => { 
+        if (criteria.member.leader){
+          if (criteria.member.leader.id == localStorage.getItem("user_id")){
+            this.filterLed.push(criteria)
+          }
+        }
+      })
     },
 
     formatPercentage(item) {
