287c0ba9c0d125a421d575fbdc3963e1737e4178
oGuimaraes <otaviovgsr@gmail.com>
2020-06-10 08:25:33 -0300

    Membros: Loader tabela - Remoção Console.logs

--- src/components/membros/Membros.vue
+++ src/components/membros/Membros.vue
@@ -1,11 +1,8 @@
 <template>
   <v-container>
     <v-snackbar top v-model="snackbar" :color="color" :timeout="timeout">
-      {{
-      text
-      }}
+      {{ text }}
     </v-snackbar>
-
 
     <v-row class="px-4">
       <h2 v-if="$route.name == 'membersAdm'">Membros</h2>
@@ -35,35 +32,53 @@
               hide-details
             ></v-text-field>
             <v-spacer></v-spacer>
-             <download-excel :fields="json_fields" :data="membros" name="membros.xls" >
-
-            <v-btn icon>
-              <v-icon>mdi-file-excel</v-icon>
-            </v-btn>
+            <download-excel
+              :fields="json_fields"
+              :data="membros"
+              name="membros.xls"
+            >
+              <v-btn icon>
+                <v-icon>mdi-file-excel</v-icon>
+              </v-btn>
             </download-excel>
           </v-card-title>
           <v-data-table
             no-data-text="Nenhum membro cadastrado"
             no-results-text="Nenhum membro encontrado"
-            :headers="($route.name == 'membersAdm') ? headersMembros : headerContactList"
+            :headers="
+              $route.name == 'membersAdm' ? headersMembros : headerContactList
+            "
             :items="membros"
             :search="search"
             :sort-by="['is_active', 'full_name']"
             :sort-desc="[true, false]"
             @click:row="memberShow"
+            :loading="showBar"
+            loading-text="Carregando..."
             class="dataTable"
           >
             <template v-slot:item.name="{ item }">
-              <span class="font-weight-medium">{{ item.first_name }} {{ item.last_name }}</span>
-              <span class="font-italic font-weight-light">{{ showNickName(item) }}</span>
+              <span class="font-weight-medium"
+                >{{ item.first_name }} {{ item.last_name }}</span
+              >
+              <span class="font-italic font-weight-light">{{
+                showNickName(item)
+              }}</span>
             </template>
             <template v-slot:item.leader="{ item }">
               <span v-if="item.leader">{{ item.leader.first_name }}</span>
               <span v-else>-</span>
             </template>
-            <template v-slot:item.is_active="{ item }" v-if="$route.name == 'membersAdm'">
-              <v-icon small v-if="item.is_active" class="success--text">mdi-brightness-1</v-icon>
-              <v-icon small v-if="!item.is_active" class="error--text">mdi-brightness-1</v-icon>
+            <template
+              v-slot:item.is_active="{ item }"
+              v-if="$route.name == 'membersAdm'"
+            >
+              <v-icon small v-if="item.is_active" class="success--text"
+                >mdi-brightness-1</v-icon
+              >
+              <v-icon small v-if="!item.is_active" class="error--text"
+                >mdi-brightness-1</v-icon
+              >
             </template>
           </v-data-table>
         </v-card>
@@ -93,20 +108,19 @@ import modalDetail from "./ModalDetail.vue";
 export default {
   components: {
     NovoMembro,
-    modalDetail
+    modalDetail,
   },
 
   async created() {
-    //this.membros = await this.getMembers()
     this.membros = this.setFullName(await this.getMembers());
-    console.log(this.membros)
-    //this.membros = this.setFullName(this.membros);
+    this.showBar = false;
   },
 
   data() {
     return {
       btnMembro: false,
-      showNickName: membro => {
+      showBar: true,
+      showNickName: (membro) => {
         if (membro.nickname) {
           return `(${membro.nickname})`;
         }
@@ -117,22 +131,22 @@ export default {
         {
           text: "Nome",
           align: "start",
-          value: "full_name"
+          value: "full_name",
         },
         { text: "Cargo", value: "post.abbreviation" },
         { text: "Área", value: "department.abbreviation" },
         { text: "Líder", value: "leader.first_name" },
-        { text: "Ativo", value: "is_active" }
+        { text: "Ativo", value: "is_active" },
       ],
       headerContactList: [
         {
           text: "Nome",
           align: "start",
-          value: "full_name"
+          value: "full_name",
         },
         { text: "Cargo", value: "post.abbreviation" },
         { text: "Área", value: "department.abbreviation" },
-        { text: "Líder", value: "leader.first_name" }
+        { text: "Líder", value: "leader.first_name" },
       ],
       membros: [],
       userDetail: null,
@@ -142,28 +156,28 @@ export default {
       timeout: 3000,
       color: "",
       json_fields: {
-        "Nome": "full_name",
+        Nome: "full_name",
         "E-mail": "email",
-        "Cargo": "post.abbreviation",
-        "Area": "department.abbreviation",
-        "Líder": {
-            callback: (value) => {
-                return `${value.leader.first_name} ${value.leader.last_name}`;
-            }
+        Cargo: "post.abbreviation",
+        Area: "department.abbreviation",
+        Líder: {
+          callback: (value) => {
+            return `${value.leader.first_name} ${value.leader.last_name}`;
+          },
         },
-        "Telefone": "phone",
-        "Slack": "slack",
-        "Apelido": "nickname",
-        "Ativo": {
-            callback: (value) => {
-              if (value.is_active)  {
-                return 'Ativo';
-              } 
-              return 'Inativo';
+        Telefone: "phone",
+        Slack: "slack",
+        Apelido: "nickname",
+        Ativo: {
+          callback: (value) => {
+            if (value.is_active) {
+              return "Ativo";
             }
+            return "Inativo";
+          },
         },
-        "Data da entrada": "date_joined"
-      }
+        "Data da entrada": "date_joined",
+      },
     };
   },
 
@@ -189,12 +203,12 @@ export default {
 
     async getMembers() {
       const members = await this.memberController.getAllMembers(this.$api);
-      members.forEach(member => {
+      members.forEach((member) => {
         if (!member.leader) {
           member.leader = { first_name: "-" };
         }
       });
-      
+
       if (this.$route.name == "membersAdm") {
         return members;
       } else {
@@ -203,7 +217,7 @@ export default {
     },
     activeMembers(members) {
       const activeMembers = new Array();
-      members.forEach(member => {
+      members.forEach((member) => {
         if (member.is_active) {
           activeMembers.push(member);
         }
@@ -212,13 +226,13 @@ export default {
     },
     setFullName(array) {
       const newArray = new Array();
-      array.map(item => {
+      array.map((item) => {
         item.full_name = `${item.first_name} ${item.last_name}`;
         newArray.push(item);
       });
       return newArray;
-    }
-  }
+    },
+  },
 };
 </script>
 
